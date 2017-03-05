aa114305e0068cc8c623d4a2f78e176abf8cb3d2
ymartyni <rura95@gmail.com>
2017-03-05 20:29:12 +0200

    add two function : 1.Create new phone 2.Show all

--- main.py
+++ main.py
@@ -5,3 +5,30 @@
 # 5. delete by name
 # 6. show all
 
+db  = {}
+#dictionary
+while True:
+    print '---------------------------------'
+    print '1. Create new phone'
+    print '6. Show all'
+    print '---------------------------------'
+
+    choice = int(raw_input())
+
+    if choice == 1:
+        print 'Creating new phone in phonebook...'
+        name = raw_input('Please type name: ')
+        phone = int(raw_input('Please type phone'))
+        db[name] = phone
+        print "{} : {}".format(name, phone)
+
+    elif choice == 6:
+        for key, value in db.items():
+            print "{} : {}".format(key, value)
+
+
+
+
+
+
+
