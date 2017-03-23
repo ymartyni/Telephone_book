f77af7bdcc0518e5fccb06bdae9d42b410ae35d9
ymartyni <rura95@gmail.com>
2017-03-23 22:16:19 +0200

    -

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ while True:
         name = raw_input('Please type name: ')
         phone = int(raw_input('Please type phone -'))
         db[name] = phone
-        print "{} : {}".format(name, phone)
-
+        print "{} : {}".format(name, phone)z
     elif choice == 6:
         for key, value in db.items():
             print "{} : {}".format(key, value)
